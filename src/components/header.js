9ba2bb47b5596607ed25806a4246b6e0bc4bd5b1
rithesh.vijayakumar <rithesh.v@lloydsbanking.com>
2020-06-21 17:31:34 +0100

    third commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Contact from './contact';
 import About from './about';
+
+import Grid from '@material-ui/core/Grid';
+import Switch from '@material-ui/core/Switch';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +24,16 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: false,
+  });
+
+  //const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
   return (
     <div className={classes.root}>
@@ -28,6 +42,11 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Indian Products
           </Typography>
+          <Grid item>English</Grid>
+          <Grid item>
+            <Switch color='primary' checked={state.checkedB} onChange={handleChange} name="checkedB" />
+          </Grid>
+          <Grid item>Kannada</Grid>
           <About/>
           <Contact/>
         </Toolbar>
